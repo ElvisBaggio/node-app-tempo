9f0a2ec22f9b631688389dd5d5e750365ba2d9fb
Elvis Oliveira <elvis.oliveira@maplink.global>
2019-06-01 16:11:26 -0300

    Ajuste app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javasctipt carredado')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
